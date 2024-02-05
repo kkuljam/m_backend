ba08e1d9e31606af98a06ef7a857be02fcff34ba
kkuljam <adgrvet13@naver.com>
2024-02-06 00:47:18 +0900

    게이지

---
+++ src/연습/별찍기.java
@@ -0,0 +1,23 @@
+package 연습;
+
+import java.util.Scanner;
+
+public class 별찍기 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /*while (true) {
+            System.out.println("체력 숫자?");
+            int hp = scanner.nextInt();
+
+            for (int i = 0; i < hp / 10; i++) {
+                System.out.print("ㅁ");
+            }
+            if(hp%10>4){
+                System.out.print("◐");
+            }
+            for (int i = 0; i < 10 - (hp % 10 > 4 ? hp / 10 + 1 : hp / 10); i++) {
+                System.out.print("○");
+            }
+        }*/
+    }
+}
